4c409b3e8d4d611282a8f9bbd560a3df76c07da2
GOODWOOLF <electromolot@mail.ru>
2026-06-08 01:10:07 +0500

    +-

--- Assets/Scripts/SimulationSystem/RobotSimulation/PhysicsEndEffector/Zajim.cs
+++ Assets/Scripts/SimulationSystem/RobotSimulation/PhysicsEndEffector/Zajim.cs
@@ -6,39 +6,38 @@ using UnityEngine;
 
 public class Gripper : MonoBehaviour
 {
+    [Header("References")]
     public RobotPropertyProvider _propertyProvider;
-
     public GameObject _leftFinger;
     public GameObject _leftFingerTrig;
     public GameObject _rightFinger;
     public GameObject _rightFingerTrig;
 
+    [Header("Settings")]
+    public float openPosition = 0f;
+    public float closePosition = 0.5f;
+    public float stiffness = 80f;        // УМЕНЬШИЛИ
+    public float damping = 15f;          // УМЕНЬШИЛИ
+    public float maxForce = 1500f;       // Сильно уменьшили
+    public float releasePushForce = 300f; // Сила, чтобы "оттолкнуть" пальцы при разжатии
+
     private Rigidbody leftRb;
     private Rigidbody rightRb;
-
     private Vector3 leftStartPos;
     private Vector3 rightStartPos;
-
-    public float openPosition = 0f;      // открыто
-    public float closePosition = 0.5f;   // закрыто (в метрах!)
-
-    public float stiffness = 140f;       // Жёсткость
-    public float damping = 22f;          // Демпфирование (против дрожи)
-    public float maxForce = 3000f;       // Максимальная сила толчка
 
     private bool detailInGrip = false;
     private GameObject Detail;
 
     private EventBus _eventBus;
-    private NotificationSystemManager _notification;
-
     private Dictionary<GameObject, List<GameObject>> collisionObjects = new();
-    private Dictionary<GameObject, Rigidbody> FingerDictionary = new();
+
+    private bool isReleasing = false;
+    private float releaseTimer = 0f;
 
     private void Start()
     {
         _eventBus = ServiceManager.Current.Get<EventBus>();
-        _notification = ServiceManager.Current.Get<NotificationSystemManager>();
 
         leftRb = _leftFinger.GetComponent<Rigidbody>();
         rightRb = _rightFinger.GetComponent<Rigidbody>();
@@ -51,51 +50,41 @@ public class Gripper : MonoBehaviour
 
         _eventBus.Subscribe<RobotCollisionEvent>(EnterCollision);
         _eventBus.Subscribe<RobotCollisionExitEvent>(ExitCollision);
-        FingerDictionary.Add(_leftFinger, leftRb);
-        FingerDictionary.Add(_rightFinger, rightRb);
     }
-
 
     void EnterCollision(RobotCollisionEvent s)
     {
-        if (s.Object == _leftFingerTrig || s.Object == _rightFingerTrig)
+        if (collisionObjects.ContainsKey(s.Object))
         {
             if (!collisionObjects[s.Object].Contains(s.CollidedObject))
-            {
                 collisionObjects[s.Object].Add(s.CollidedObject);
-            }
         }
     }
 
     void ExitCollision(RobotCollisionExitEvent s)
     {
-        if (s.Object == _leftFingerTrig || s.Object == _rightFingerTrig)
-        {
+        if (collisionObjects.ContainsKey(s.Object))
             collisionObjects[s.Object].Remove(s.CollidedObject);
-        }
     }
 
     void FixedUpdate()
     {
-        bool grip = _propertyProvider.EndEffectorOn;   // true = закрывать
+        bool gripCommand = _propertyProvider.EndEffectorOn;
 
-        float targetLeft = grip ? closePosition : openPosition;
-        float targetRight = grip ? closePosition : openPosition;
+        float target = gripCommand ? closePosition : openPosition;
 
-        MoveFinger(leftRb, leftStartPos, targetLeft);
-        MoveFinger(rightRb, rightStartPos, targetRight);
+        MoveFinger(leftRb, leftStartPos, target);
+        MoveFinger(rightRb, rightStartPos, target);
 
-        // Логика захвата детали
-        HandleGripLogic(grip);
-
-
+        HandleGripLogic(gripCommand);
     }
 
     private void MoveFinger(Rigidbody rb, Vector3 startLocalPos, float targetX)
     {
-        Vector3 targetWorld = rb.transform.parent.TransformPoint(startLocalPos + new Vector3(targetX, 0, 0));
-        Vector3 error = targetWorld - rb.worldCenterOfMass;
+        Vector3 targetLocal = startLocalPos + new Vector3(targetX, 0, 0);
+        Vector3 targetWorld = rb.transform.parent.TransformPoint(targetLocal);
 
+        Vector3 error = targetWorld - rb.worldCenterOfMass;
         Vector3 targetVel = error * stiffness;
         Vector3 velError = targetVel - rb.linearVelocity;
 
@@ -104,40 +93,68 @@ public class Gripper : MonoBehaviour
         if (force.magnitude > maxForce)
             force = force.normalized * maxForce;
 
+        // При разжатии добавляем небольшую отталкивающую силу
+        if (!_propertyProvider.EndEffectorOn)
+            force += (rb.transform.position - transform.position).normalized * releasePushForce * 0.3f;
+
         rb.AddForce(force, ForceMode.Acceleration);
     }
 
-    private void HandleGripLogic(bool grip)
+    private void HandleGripLogic(bool gripCommand)
     {
-        if (grip && !detailInGrip)
+        if (gripCommand && !detailInGrip)
         {
-            // Проверяем, зажали ли деталь с двух сторон
-            foreach (var obj in collisionObjects[_leftFingerTrig])
+            TryGrabObject();
+        }
+        else if (!gripCommand && detailInGrip)
+        {
+            ReleaseObject();
+        }
+    }
+
+    private void TryGrabObject()
+    {
+        foreach (var obj in collisionObjects[_leftFingerTrig])
+        {
+            if (obj.layer == LayerMask.NameToLayer("Detail") &&
+                collisionObjects[_rightFingerTrig].Contains(obj))
             {
-                if (obj.layer == LayerMask.NameToLayer("Detail") &&
-                    collisionObjects[_rightFingerTrig].Contains(obj))
+                Detail = obj;
+                var detailRb = Detail.GetComponent<Rigidbody>();
+
+                if (detailRb != null)
                 {
-                    detailInGrip = true;
-                    Detail = obj;
-                    Detail.GetComponent<Rigidbody>().isKinematic = true;
+                    detailRb.isKinematic = true;
                     Detail.transform.SetParent(transform);
-                    break;
                 }
+
+                detailInGrip = true;
+                break;
             }
         }
-        else if (!grip && detailInGrip)
+    }
+
+    private void ReleaseObject()
+    {
+        if (Detail == null)
         {
-            if (Detail != null)
-            {
-                
-                if (Detail.transform.parent == transform)
-                {
-                    Detail.GetComponent<Rigidbody>().isKinematic = false;
-                    Detail.transform.SetParent(null);
-                }
-            }
             detailInGrip = false;
-            Detail = null;
+            return;
         }
+
+        if (Detail.transform.parent == transform)
+        {
+            Detail.transform.SetParent(null);
+            var detailRb = Detail.GetComponent<Rigidbody>();
+            if (detailRb != null)
+            {
+                detailRb.isKinematic = false;
+                // Небольшой импульс вниз, чтобы деталь "отлепилась"
+                detailRb.AddForce(Vector3.down * 0.5f, ForceMode.Impulse);
+            }
+        }
+
+        detailInGrip = false;
+        Detail = null;
     }
 }
